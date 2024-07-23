d99070886391b709997244750b9cb56ad58ec5b5
Brian <brian.w.tjahjadi@gmail.com>
2024-07-24 02:00:15 +1000

    modularization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,61 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { MoveDirectionContext } from '../contexts/moveDirectionContext.jsx'
 import { TileDataContext } from '../contexts/tileDataContext.jsx';
 import { BOARD_DIMENSIONS } from './constants'
-
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
-}
-
-
-const boardLeftMove = (prevTileData) => {
-  const newTileData = [...prevTileData];
-  for (let i = 1; i < newTileData.length; i++){
-    if (i % (BOARD_DIMENSIONS) === 0){
-      continue;
-    }
-    if (newTileData[i-1] === 0 && newTileData[i] === 1){
-      newTileData[i-1] = 1;
-      newTileData[i] = 0;
-    }
-  }
-  return newTileData;
-}
-
-const boardRightMove = (prevTileData) => {
-  const newTileData = [...prevTileData];
-  for (let i = newTileData.length-2; i > -1; i--){
-    if (i % BOARD_DIMENSIONS === (BOARD_DIMENSIONS - 1)){
-      continue;
-    }
-    if (newTileData[i+1] === 0 && newTileData[i] === 1){
-      newTileData[i+1] = 1;
-      newTileData[i] = 0;
-    }
-  }
-  return newTileData;
-}
-
-const boardUpMove = (prevTileData) => {
-  const newTileData = [...prevTileData];
-  for (let i = BOARD_DIMENSIONS; i < newTileData.length; i++){
-    if (newTileData[i-BOARD_DIMENSIONS] === 0 && newTileData[i] === 1){
-      newTileData[i-BOARD_DIMENSIONS] = 1;
-      newTileData[i] = 0;
-    }
-  }
-  return newTileData;
-}
-
-const boardDownMove = (prevTileData) => {
-  const newTileData = [...prevTileData];
-  for (let i = newTileData.length - BOARD_DIMENSIONS - 1; i >= 0; i--) {
-    if (newTileData[i + BOARD_DIMENSIONS] === 0 && newTileData[i] === 1) {
-      newTileData[i + BOARD_DIMENSIONS] = 1;
-      newTileData[i] = 0;
-    }
-  }
-  return newTileData;
-}
+import { getRandomInt, boardLeftMove, boardRightMove, boardDownMove, boardUpMove } from '../scripts/boardHelpers.jsx';
 
 /* 
 * Board tiles:
